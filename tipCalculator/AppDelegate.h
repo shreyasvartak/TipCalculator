832a1cdbe3bdb7d42ef3e901d29a886e4eade2b4
Shreyas Vartak <svartak@yahoo-inc.com>
2014-09-07 11:13:53 -0700

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Shreyas Vartak on 9/7/14.
+//  Copyright (c) 2014 Shreyas Vartak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipCalculator
+//
+//  Created by Shreyas Vartak on 9/7/14.
+//  Copyright (c) 2014 Shreyas Vartak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
